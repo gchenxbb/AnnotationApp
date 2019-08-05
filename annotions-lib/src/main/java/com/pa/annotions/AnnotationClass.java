206fd743910d6196535714f8fd093d36f9136fa8
Nico <chenguang0508@163.com>
2019-08-05 23:49:56 +0800

    annotation processor

--- annotions-lib/src/main/java/com/pa/annotions/AnnotationClass.java
+++ annotions-lib/src/main/java/com/pa/annotions/AnnotationClass.java
@@ -4,10 +4,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.CLASS;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-//改为运行时注解，
+//运行时注解
 //作用范围是类
 @Target(TYPE)
 @Retention(RUNTIME)//
